31c2aa328b9e7a2bbac40755fa9c73950b1f1ebe
Yang, Bo <bo@preemo.io>
2023-11-08 21:52:22 +0000

    Use cloud-init for provisioning

--- nixos/modules/virtualisation/azure-agent.nix
+++ nixos/modules/virtualisation/azure-agent.nix
@@ -46,6 +46,8 @@ in
     boot.initrd.kernelModules = [ "ata_piix" ];
     networking.firewall.allowedUDPPorts = [ 68 ];
 
+    # Use cloud-init for provisioning
+    services.cloud-init.enable = true;
 
     environment.etc."waagent.conf".text = ''
         #
@@ -61,7 +63,7 @@ in
 
         # Which provisioning agent to use. Supported values are "auto" (default), "waagent",
         # "cloud-init", or "disabled".
-        Provisioning.Agent=auto
+        Provisioning.Agent=cloud-init
 
         # Password authentication for root account will be unavailable.
         Provisioning.DeleteRootPassword=n
